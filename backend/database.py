25fc0126a64a3d95a05f7919f2af7b14cead5d55
FarahKhattab <117682920+FarahKhattab@users.noreply.github.com>
2025-05-31 03:58:47 +0200

    fixed bug

--- backend/database.py
+++ backend/database.py
@@ -5,9 +5,9 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL",connect_args={"sslmode": "require"})
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,connect_args={"sslmode": "require"})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
